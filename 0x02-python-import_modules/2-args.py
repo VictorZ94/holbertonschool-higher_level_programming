21e4bf8150dd48b70c18408b83b31aae1f5ec353
VictorZ94 <zrvictor@outlook.com>
2020-09-03 20:53:35 +0000

    argiment to main in python

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import sys
+i = 1
+args = sys.argv[1:]
+print("{} arguments:".format(len(sys.argv) -1))
+for arguments in args:
+    print("{}: {}".format(i , str(arguments)))
+    i += 1
