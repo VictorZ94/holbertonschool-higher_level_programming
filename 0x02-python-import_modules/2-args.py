cd38dac11dbabbf9a97ed6302f4222f0fac82b4e
VictorZ94 <zrvictor@outlook.com>
2020-09-03 21:42:39 +0000

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-args = len(sys.argv) - 1
-if args == 0:
-    print("{} arguments.".format(args))
-elif args == 1:
-    print("{} argument.".format(args))
-else:
-    print("{} arguments:".format(args))
-for index in range(len(sys.argv)):
-    if (index == 0):
-        continue
-    print("{}: {}".format(index, sys.argv[index]))
+if __name__ == "__main__":
+    import sys
+    args = len(sys.argv) - 1
+    if args == 0:
+        print("{} arguments.".format(args))
+    elif args == 1:
+        print("{} argument.".format(args))
+    else:
+        print("{} arguments:".format(args))
+    for index in range(len(sys.argv)):
+        if (index == 0):
+            continue
+        print("{}: {}".format(index, sys.argv[index]))
