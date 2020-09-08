358af55115a5558c165ab84ce2f95ebc9a350041
VictorZ94 <zrvictor@outlook.com>
2020-09-08 20:57:40 +0000

    quit bucle

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx >= 0 and idx <= len(my_list):
-        i = 0
-        while i < idx:
-            i += 1
-            continue
-        return (my_list[i])
+    if idx >= 0 and idx < len(my_list):
+        return (my_list[idx])
     else:
         return ('None')
