6bfd8e0302bb76e3ab9913c9fead7f07c8ed5dce
VictorZ94 <zrvictor@outlook.com>
2020-09-08 20:46:47 +0000

    return element at the index given

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
-        return ('None')
-    elif idx > len(my_list):
-        return ('None')
-    else:
+    if idx >= 0 and idx <= len(my_list):
         i = 0
         while i < idx:
             i += 1
             continue
         return (my_list[i])
+    else:
+        return ('None')
