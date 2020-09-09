9f6a3d4021eb465830bebce069b1d35904355d5d
Victor Zuluaga <zrvictor@outlook.com>
2020-09-09 16:23:36 -0500

    divisible

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     New_list = []
     for i in my_list:
-        if my_list[i] % 2 == 0:
+        if i % 2 == 0:
             New_list += [True]
         else:
             New_list += [False]
