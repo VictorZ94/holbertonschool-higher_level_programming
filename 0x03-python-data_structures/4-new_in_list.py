efdf59c36cb098fca0fc4535c31a3b1ad118b7ba
Victor Zuluaga <zrvictor@outlook.com>
2020-09-08 21:45:16 -0500

    is statament control

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    Newlist = list.copy(my_list)
-    Newlist[idx] = element
-    return (Newlist)
+    if idx >= 0 and idx < len(my_list):
+        Newlist = list.copy(my_list)
+        Newlist[idx] = element
+        return (Newlist)
+    else:
+        return ('None')
