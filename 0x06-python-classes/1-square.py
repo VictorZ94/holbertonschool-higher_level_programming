73d3915a9fbf80f39b6c313a18327ac78282df13
Victor Zuluaga <zrvictor@outlook.com>
2020-09-22 18:34:19 -0500

    square with attribute privatly

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Class square 
+"""
+
+
+class Square:
+    """Method to proof privatly an attribute"""
+    def __init__(self, size):
+        """function to receive size parameter
+        Args:
+            size (int): define the size of the square.
+        """
+        self._size = size
