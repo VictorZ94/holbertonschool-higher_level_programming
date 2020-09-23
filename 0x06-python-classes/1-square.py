9694ec36829e4f817ae9b51593efb9e3bd3d2ea8
Victor Zuluaga <zrvictor@outlook.com>
2020-09-22 20:29:48 -0500

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,12 +6,13 @@ Class square
 
 class Square:
     """Method to proof privatly an attribute
-    Args:
-        size (int): define the size of the square.
+    
+        Args:
+            size (int): define the size of the square.
     """
     def __init__(self, size):
         """function to receive size parameter
-        Args:
-            size (int): define the size of the square.
+            Args:
+                size (int): define the size of the square.
         """
         self._Square__size = size
