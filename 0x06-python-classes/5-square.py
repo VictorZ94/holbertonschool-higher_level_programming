68e7302127a7a10a3f9f7ded925fd55142cc21cf
Victor Zuluaga <zrvictor@outlook.com>
2020-09-23 07:58:10 -0500

    print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Class square - Print #
+"""
+
+
+class Square:
+    """Method to proof privatly an attribute
+    """
+    def __init__(self, size=0):
+        '''function to receive size parameter
+            Args:
+                size (int): define the size of the square.
+        '''
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
