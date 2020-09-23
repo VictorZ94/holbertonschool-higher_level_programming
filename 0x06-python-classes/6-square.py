0bd234297a83a062e6b7989fe666429441a124f6
Victor Zuluaga <zrvictor@outlook.com>
2020-09-23 14:03:20 -0500

    Square v6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+class Square:
+    """Method to proof privatly an attribute
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value):
+        if type(value[0]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for i in range(self.__position[1]):
+            print()
+        
+        for j in range(self.__size):
+            for y in range(self.__position[0]):
+                print(" ", end="")
+            for x in range(self.__size):
+                print("#", end="")
+            print()
+        if self.__size == 0:
+            print()
