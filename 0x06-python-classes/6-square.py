2ac6c1c4d3515a0c8922514504184f3419e37ec4
Victor Zuluaga <zrvictor@outlook.com>
2020-09-23 14:21:02 -0500

    Square v6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,8 +7,8 @@ class Square:
     """Method to proof privatly an attribute
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -28,9 +28,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if type(value[0]) is not int:
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[1]) is not int:
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
