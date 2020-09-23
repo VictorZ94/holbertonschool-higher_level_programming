5f7bfc9aa1d904e921fbb7844cdd606e014ea4f8
Victor Zuluaga <zrvictor@outlook.com>
2020-09-23 18:55:46 -0500

    Square v7

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Class square - computer area.
+"""
+
+
+class Square:
+    """Method to proof privatly an attribute
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+                print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+
+            for j in range(self.__size):
+                for y in range(self.__position[0]):
+                    print(" ", end="")
+                for x in range(self.__size):
+                    print("#", end="")
+                print()
