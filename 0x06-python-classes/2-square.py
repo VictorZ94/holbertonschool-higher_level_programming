29df58cf2e11489aa57ed008bc963ee94cc43e9e
Victor Zuluaga <zrvictor@outlook.com>
2020-09-22 20:36:04 -0500

    square 2 version

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Class square
+"""
+
+
+class Square:
+    """Method to proof privatly an attribute
+    """
+    def __init__(self, size=0):
+        '''function to receive size parameter
+            Args:
+                size (int): define the size of the square.
+        '''
+        if isinstance(size, str):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
