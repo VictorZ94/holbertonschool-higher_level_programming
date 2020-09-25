2c56635a07cef280314e21d36317eaa89d444b40
Victor Zuluaga <zrvictor@outlook.com>
2020-09-25 11:35:58 -0500

    matrix divided

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""[summary]
+"""
+
+
+def matrix_divided(matrix, div):
+    """[summary]
+
+    Args:
+        matrix ([type]): [description]
+        div ([type]): [description]
+
+    Raises:
+        TypeError: [description]
+        TypeError: [description]
+        TypeError: [description]
+        TypeError: [description]
+        ZeroDivisionError: [description]
+
+    Returns:
+        [type]: [description]
+    """
+    # check all values be on int or float 
+    j = 0
+    for y in matrix:
+        if type(matrix) is not list or type(matrix[j]) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        for x in matrix[j]:
+            if type(x) is not int and type(x) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        j += 1
+
+    size = len(matrix[0])
+    j = 0
+    for y in matrix:
+        lenght = len(matrix[j])
+        if lenght != size:
+            raise TypeError("Each row of the matrix must have the same size")
+        j += 1
+
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    NewMatrix = []
+    i = 0
+    for y in matrix:
+        NewMatrix.append([])
+        for x in matrix[i]:
+            NewMatrix[i].append(round((x / div), 2))
+        i += 1
+    return (NewMatrix)
