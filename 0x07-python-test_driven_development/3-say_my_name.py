783cc3f52ff1808cfb3a3d8cc42cd78e85defda2
Victor Zuluaga <zrvictor@outlook.com>
2020-09-27 18:48:16 -0500

    say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -14,11 +14,8 @@ def say_my_name(first_name, last_name=""):
         TypeError: [description]
         TypeError: [description]
     """
-    if first_name == "" and last_name == "":
-        raise TypeError("first_name must be a string")
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    else:
-        print("My name is {:s} {:s}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
