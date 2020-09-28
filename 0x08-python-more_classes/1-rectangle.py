750bb81e28d4729840151209d2188afe1ca959b5
Victor Zuluaga <zrvictor@outlook.com>
2020-09-28 18:17:07 -0500

    Dict organized

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,15 +6,15 @@
 class Rectangle:
     """ Doc """
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
-    def width(self): # getter
+    def width(self):  # getter
         return self.__width
 
     @width.setter
-    def width(self, value): # setter
+    def width(self, value):  # setter
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
