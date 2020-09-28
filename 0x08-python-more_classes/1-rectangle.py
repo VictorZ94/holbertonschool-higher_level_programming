24e955d38e08ca846b6e9095ccf7dc419f7af6ac
Victor Zuluaga <zrvictor@outlook.com>
2020-09-28 18:21:35 -0500

    updated rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):  # setter
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -27,7 +27,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
