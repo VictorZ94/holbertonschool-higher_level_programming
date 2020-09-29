3ae4429b369fca2bd2106599c476be3cb135721f
Victor Zuluaga <zrvictor@outlook.com>
2020-09-28 19:09:15 -0500

    rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -6,9 +6,9 @@
 class Rectangle:
     """ Doc """
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
-        
+        self.height = height
+        self.width = width
+
     @property
     def width(self):  # getter
         return self.__width
