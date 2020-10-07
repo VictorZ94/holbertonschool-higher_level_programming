1724162ecbcdad1ff3b3366be40e155943e9a179
Victor Zuluaga <zrvictor@outlook.com>
2020-10-07 17:06:03 -0500

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function to read a file
+"""
+
+
+def read_file(filename=""):
+    """print read file
+
+    Args:
+        filename (pathname, format or mode) Defaults to "".
+    """
+    with open(filename, encoding="utf-8") as myFile:
+        print(myFile.read())
