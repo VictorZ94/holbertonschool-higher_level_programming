43d18b5bb2e290516cdd4c8bacd3388bddebcb1f
Victor Zuluaga <zrvictor@outlook.com>
2020-10-06 21:00:46 -0500

    rectangle class 1

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -28,8 +28,6 @@ class Rectangle(BaseGeometry):
         """Doc"""
         self.__width = width
         self.__height = height
-        # self.integer_validator("width" , self.__width)
-        # self.integer_validator("height", self.__height)
         super().integer_validator("width", self.__width)
         super().integer_validator("height", self.__height)
 
