483f59de06c54912c74a1bc54b8c20f8c74776e3
Victor Zuluaga <zrvictor@outlook.com>
2020-10-06 21:45:50 -0500

    inherits from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Doc
+"""
+
+
 def inherits_from(obj, a_class):
+    """Doc"""
     if type(obj) is not a_class and issubclass(type(obj), a_class):
         return True
     else:
