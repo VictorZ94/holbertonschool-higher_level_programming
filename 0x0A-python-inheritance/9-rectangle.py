b9ca4d86d52abb7e8a4a9089eb4aaf4f3a24c3fa
Victor Zuluaga <zrvictor@outlook.com>
2020-10-06 20:58:44 -0500

    rectangle class 1

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Doc
+"""
+
+
+class BaseGeometry:
+    """Doc"""
+    def area(self):
+        """Doc"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Doc"""
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+        self.name = name
+        self.value = value
+
+"""Doc
+"""
+
+
+class Rectangle(BaseGeometry):
+    """Doc"""
+    def __init__(self, width, height):
+        """Doc"""
+        self.__width = width
+        self.__height = height
+        # self.integer_validator("width" , self.__width)
+        # self.integer_validator("height", self.__height)
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
