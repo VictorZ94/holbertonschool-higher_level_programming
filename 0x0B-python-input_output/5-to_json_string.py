38a39980021a22c3f6f63dd77d4efb3ee787ee50
Victor Zuluaga <zrvictor@outlook.com>
2020-10-07 19:40:35 -0500

    to Json string

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""To change JSON to string
+"""
+
+
+import json
+def to_json_string(my_obj):
+    """
+    Args:
+        my_obj ([type]): [description]
+    Returns:
+        object: return data type
+    """
+    string = json.dumps(my_obj)
+    return string
