28fdcf7fb10a37bac6d668c02b1fd1a8f93beec4
Victor Zuluaga <zrvictor@outlook.com>
2020-10-08 20:53:55 -0500

    student 2

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Declare new class
+"""
+
+
+class Student:
+    """Doc"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        dic = {}
+        if type(attrs) is not list:
+            return self.__dict__
+        else:
+            for i in attrs:
+                if i in self.__dict__:
+                    dic[i] = self.__dict__[i]
+            return dic
