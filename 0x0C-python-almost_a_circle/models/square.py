5bff7c02b221414f28f4d79340e98b878753d321
Victor Zuluaga <zrvictor@outlook.com>
2020-10-12 14:40:53 -0500

    square v0.1

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Class Square that inherits from rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Args:
+        Rectangle (class): [parameter intherited]
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Args:
+            size (int): [value of width or height]
+            x (int): [(horizontal) direction is called X]. Defaults to 0.
+            y (int): [(vertical) direction is called Y]. Defaults to 0.
+            id (int): [value id]. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}".format(
+             self.__class__.__name__, self.id, self.x, self.y, self.width)
