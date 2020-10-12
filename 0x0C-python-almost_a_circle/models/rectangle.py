011578d5f21df0f610ac37cb5fe059615695fb41
Victor Zuluaga <zrvictor@outlook.com>
2020-10-12 14:35:51 -0500

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -87,8 +87,9 @@ class Rectangle(Base):
 
     def __str__(self):
         """overwriting method __str__ """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-             self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[{}] ({}) {}/{} - {}/{}".format(
+             self.__class__.__name__, self.id, self.__x, self.__y,
+             self.__width, self.__height)
 
     def update(self, *args, **kwargs):
         if len(args) != 0:
