721924bc2a0858fe7e222170639a88f62a3965e5
Victor Zuluaga <zrvictor@outlook.com>
2020-10-12 20:22:42 -0500

    base v0.4

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Class Base Father
 """
+import json
 
 
 class Base:
@@ -13,3 +14,25 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None or list_objs == []:
+            Newlist = []
+        else:
+            Newlist = cls.to_json_string([obj.to_dictionary()
+                                          for obj in list_objs])
+
+        with open(cls.__name__ + ".json", "w") as myFile:
+            myFile.write(Newlist)
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
