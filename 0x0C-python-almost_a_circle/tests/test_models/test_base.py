5ccb908ce1376150f3cda3391a48b58d7f994e06
Victor Zuluaga <zrvictor@outlook.com>
2020-10-13 19:11:55 -0500

    test to class base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Unittest for class Base(obj)
 """
-import unittest
 from models.base import Base
+import unittest
+# import inspect
 
 
 class Testbase(unittest.TestCase):
-    # """Test base class"""
+    """Test base class"""
     # def test_pep8(self):
     #     """Test that we conform to PEP8."""
     #     pep8style = pep8.StyleGuide(quiet=True)
@@ -14,15 +15,34 @@ class Testbase(unittest.TestCase):
     #     self.assertEqual(result.total_errors, 0,
     #                      "Found code style errors (and warnings).")
 
-    def test_assert_equal(self):
+    # def test_doctstring(self):
+    #     """Checks doctstring for base class"""
+    #     print(mrdoc)
+    #     self.assertTrue(len(mrdoc.strip()) > 0)
+    #     self.assertTrue(len(Base.__doc__.strip()) > 0)
+    #     functions = inspect.getmembers(Base, predicate=inspect.ismethod)
+    #     for name, func in functions:
+    #         self.assertTrue(len(func.__doc__.strip()) > 0)
+    #     functions = inspect.getmembers(Base, predicate=inspect.isfunction)
+    #     for name, func in functions:
+    #         self.assertTrue(len(func.__doc__.strip()) > 0)
+
+    def test_assert_equal1(self):
         b1 = Base()
-        b2 = Base()
-        b3 = Base()
-        b4 = Base(12)
         self.assertEqual(b1.id, 1)
+
+    def test_assert_equal2(self):
+        b2 = Base()
         self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
+
+    def test_assert_equal4(self):
+        b4 = Base(12)
         self.assertEqual(b4.id, 12)
+
+    def test_assert_equal3(self):
+        b3 = Base()
+        self.assertEqual(b3.id, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
