8b5858f79f6856937e02c351fb5b1d620193394f
Victor Zuluaga <zrvictor@outlook.com>
2020-10-13 19:14:08 -0500

    test to base class

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,38 +8,24 @@ import unittest
 
 class Testbase(unittest.TestCase):
     """Test base class"""
-    # def test_pep8(self):
-    #     """Test that we conform to PEP8."""
-    #     pep8style = pep8.StyleGuide(quiet=True)
-    #     result = pep8style.check_files(['models/base.py'])
-    #     self.assertEqual(result.total_errors, 0,
-    #                      "Found code style errors (and warnings).")
-
-    # def test_doctstring(self):
-    #     """Checks doctstring for base class"""
-    #     print(mrdoc)
-    #     self.assertTrue(len(mrdoc.strip()) > 0)
-    #     self.assertTrue(len(Base.__doc__.strip()) > 0)
-    #     functions = inspect.getmembers(Base, predicate=inspect.ismethod)
-    #     for name, func in functions:
-    #         self.assertTrue(len(func.__doc__.strip()) > 0)
-    #     functions = inspect.getmembers(Base, predicate=inspect.isfunction)
-    #     for name, func in functions:
-    #         self.assertTrue(len(func.__doc__.strip()) > 0)
 
     def test_assert_equal1(self):
+        """Normal arguments """
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
     def test_assert_equal2(self):
+        """Normal arguments """
         b2 = Base()
         self.assertEqual(b2.id, 2)
 
     def test_assert_equal4(self):
+        """Normal arguments """
         b4 = Base(12)
         self.assertEqual(b4.id, 12)
 
     def test_assert_equal3(self):
+        """Normal arguments """
         b3 = Base()
         self.assertEqual(b3.id, 3)
 
