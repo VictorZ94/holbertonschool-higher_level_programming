1921aadfa28fc8ccd364601eb37a366a88d996b0
Victor Zuluaga <zrvictor@outlook.com>
2020-10-15 18:12:55 -0500

    task advanced

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Class Base Father
 """
 import json
+import csv
 
 
 class Base:
@@ -61,3 +62,27 @@ class Base:
         for iter in listtemp:
             Newlist.append(cls.create(**iter))
         return Newlist
+
+    @classmethod
+    def load_from_file_csv(cls):
+        try:
+            with open("{}.csv".format(cls.__name__), mode="rt") as f:
+                listtemp = cls.from_json_string(f.read())
+        except:
+            listtemp = []
+
+        Newlist = []
+        for iter in listtemp:
+            Newlist.append(cls.create(**iter))
+        return Newlist
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        if list_objs is None or list_objs == []:
+            Newlist = cls.to_json_string([])
+        else:
+            Newlist = cls.to_json_string([obj.to_dictionary()
+                                          for obj in list_objs])
+
+        with open(cls.__name__ + ".csv", "w") as myFile:
+            myFile.write(Newlist)
