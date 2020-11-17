4aa5968074b0d497d00166b4adde3af5bf0d2334
Victor Zuluaga <zrvictor@outlook.com>
2020-11-17 15:34:13 -0500

    create database and table with constraints

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Create a table with constrants
+-- CREATE TABLES
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
