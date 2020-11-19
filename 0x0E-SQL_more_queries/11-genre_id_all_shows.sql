5de60fb04b04f649f0167fbad29302c77af7935d
Victor Zuluaga <zrvictor@outlook.com>
2020-11-18 20:41:25 -0500

    genre id all shows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- List all cities and states
+-- SELECT AND JOIN
+SELECT tv_s.title, tv_sg.genre_id
+FROM tv_shows AS tv_s
+LEFT JOIN tv_show_genres AS tv_sg
+ON tv_s.id=tv_sg.show_id
+ORDER BY tv_s.title, tv_sg.genre_id;
