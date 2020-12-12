16dca09ddedf4f511b5f6d13749142da6d03c325
victorZ94 <zrvictor@outlook.com>
2020-12-12 13:58:19 +0000

    safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Filter safe only state input by command line
+"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    conn = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
+                           database=sys.argv[3])
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id;")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        if row[1] == sys.argv[4]:
+            print("{}".format(row))
+    cursor.close()
+    conn.close()
