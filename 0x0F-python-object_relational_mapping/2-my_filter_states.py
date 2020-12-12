6f180c832bbbe8b69df6903c8628bc054cf865b8
victorZ94 <zrvictor@outlook.com>
2020-12-12 22:35:36 +0000

    my only states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    conn = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2],
+                           database=sys.argv[3])
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id;")
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        if row[1] == sys.argv[4]:
+            print("{}".format(row))
+    cursor.close()
+    conn.close()
