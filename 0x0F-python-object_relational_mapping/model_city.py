fc0fb99b40afb3032bdefe80c429441a1078a74f
Victor Zuluaga <zrvictor@outlook.com>
2020-12-13 18:27:55 -0500

    model city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Create Table using cities sqlalchemy
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """ class City inherits from Base
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
