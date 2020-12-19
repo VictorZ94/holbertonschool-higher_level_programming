a3b922a558e5a5beb38a07945b087a2a147ede29
Victor Zuluaga <zrvictor@outlook.com>
2020-12-18 20:01:12 -0500

    better to comments

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 #  sends a request to that URL, and displays the size of the body of the response
-curl -sI $1 | grep Content-Length | cut -d ' ' -f2
+# I'm using --silent to avoid to display the content below --silent is equal to -s
+#=================================================================================
+# % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+#                                 Dload  Upload   Total   Spent    Left  Speed
+#   0    17    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0
+#====================================================================================
+# I'm using --head to display only header --header is equal to -I
+curl --silent --head $1 | grep Content-Length | cut -d ' ' -f2
