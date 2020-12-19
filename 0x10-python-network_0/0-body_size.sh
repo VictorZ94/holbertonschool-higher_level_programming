2d8d1ed235edd4f365b512d778776f686d8655d9
Victor Zuluaga <zrvictor@outlook.com>
2020-12-18 19:43:34 -0500

    filter body size and display in bytes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  sends a request to that URL, and displays the size of the body of the response
-curl -I $1 | grep Content-Lenght | cut -d ' ' -f2
+curl -sI $1 | grep Content-Length | cut -d ' ' -f2
