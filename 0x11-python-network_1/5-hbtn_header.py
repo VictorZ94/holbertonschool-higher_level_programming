ad8153e2d0d0769d19becf95cefe33db0e6b4c79
Victor Zuluaga <zrvictor@outlook.com>
2021-01-11 16:06:09 -0500

    fix function arguments to main

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,9 +3,11 @@
     with requests library
 """
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    _request = requests.get('https://intranet.hbtn.io/status')
+    url = sys.argv[1]
+    _request = requests.get(url)
     _header = _request.headers['X-Request-Id']
     print("{}".format(_header))
