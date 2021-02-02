60a40968d79ea5273dccafbdda89b351865b63e7
Victor Zuluaga <zrvictor@outlook.com>
2021-02-02 18:24:38 -0500

    print using loop while

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// to print multi string using Console
+let iter = 0;
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+while (iter < myArray.length) {
+  console.log(myArray[iter]);
+  iter++;
+}
