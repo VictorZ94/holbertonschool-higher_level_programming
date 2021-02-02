e52eb3ded39c4692c4e55b9288ab8800aeed1cde
Victor Zuluaga <zrvictor@outlook.com>
2021-02-02 17:40:32 -0500

    conat two strings input by command line

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// concat command line arguments
+const argumen = process.argv;
+const argument1 = argumen[2];
+const argument2 = argumen[3];
+console.log(argument1 + ' is ' + argument2);
