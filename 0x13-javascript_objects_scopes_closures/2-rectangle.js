9319bfe66c6dd5e7deabd5e8b89e7e064b135e6b
Victor Zuluaga <zrvictor@outlook.com>
2021-02-08 16:01:30 -0500

    defining rectangle width and height

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      this.width;
-      this.height;
-    } else {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
