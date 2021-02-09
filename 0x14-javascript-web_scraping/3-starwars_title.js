941705a990977e081be34bc5198d5e27865a23eb
Victor Zuluaga <zrvictor@outlook.com>
2021-02-09 14:12:13 -0500

    filter JSON starwars title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  const bodyParsed = JSON.parse(body);
+  console.log(bodyParsed.title);
+});
