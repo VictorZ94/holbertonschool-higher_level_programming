32c16bb9545b5f72635d0478f55491f6a396938c
Victor Zuluaga <zrvictor@outlook.com>
2021-02-08 19:14:43 -0500

    square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 const SquareParent = require('./5-square');
 class Square extends SquareParent {
-  charPrint(c) {
+  charPrint (c) {
     if (c !== undefined) {
       for (let i = 0; i < this.width; i++) {
         console.log(c.repeat(this.height));
       }
-    }
-    else {
+    } else {
       this.print();
     }
   }
