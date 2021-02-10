7930f0c490ea740a5662597ff97110740a1e4541
Victor Zuluaga <zrvictor@outlook.com>
2021-02-10 14:59:24 -0500

    DOM manipulation

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,14 @@
+// $(function()
+// {
+//   $('header').css("color", "#FF0000");
+// });
+
+$(document).ready(function() {
+   $('header').css('color', '#FF0000');
+});
+
+// All three of the following syntaxes are equivalent:
+
+// $(document).ready(handler)
+// $().ready(handler) (this is not recommended)
+// $(handler)
